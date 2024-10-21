7c925f44a7226c4a29501643d3869c044b945b07
shinnnnnn1 <shindy0224@gmail.com>
2024-10-21 13:19:36 +0900

    Default PlayerCtrl

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody rigid;
+
+    [SerializeField] Vector3 inputDirection;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    #region INPUT
+    public void InputMove(InputAction.CallbackContext context)
+    {
+        inputDirection = context.ReadValue<Vector2>();
+    }
+    public void InputJump(InputAction.CallbackContext context)
+    {
+
+    }
+    public void InputAction(InputAction.CallbackContext context)
+    {
+
+    }
+    #endregion
+}
