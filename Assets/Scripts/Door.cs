8e32498c4b3ec0329f7c4ac4a865ca041cc7850a
shinnnnnn2 <shindy0224@gmail.com>
2024-11-27 14:28:45 +0900

    Trigger2

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -7,8 +7,21 @@ public class Door : MonoBehaviour
 {
     [SerializeField] Transform door;
 
+    Vector3 origin = new Vector3(2.5f, 1.3f, -0.2f);
+    float value = 2.5f;
+
     public void Activate(bool activate)
     {
-
+        door.DOPause();
+        if (activate)
+        {
+            float time = Mathf.Abs(door.transform.localPosition.y - value) / value;
+            door.DOLocalMove(Vector3.up * value, time).SetEase(Ease.Linear);
+        }
+        else
+        {
+            float time = Mathf.Abs(door.transform.localPosition.y) / value;
+            door.DOLocalMove(origin, time).SetEase(Ease.Linear);
+        }
     }
 }
--- Assets/Scripts/TriggerButton.cs
+++ Assets/Scripts/TriggerButton.cs
@@ -2,13 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using DG.Tweening;
 
-public class TriggerButton : MonoBehaviour
+public class TriggerButton : MonoBehaviour, IInteractable
 {
     [Space(10f)] [SerializeField] bool isFloor = true;
 
     [Space(10f)]
-    [SerializeField] UnityEvent on;
-    [SerializeField] UnityEvent off;
+    [SerializeField] UnityEvent[] on;
+    [SerializeField] UnityEvent[] off;
+
+    [SerializeField] Transform button;
+
+    Vector3 origin;
+    float value;
+
+    bool isActivated;
+
+    void Start()
+    {
+        origin = button.position;
+    }
+
+    public void Interact(PlayerCtrl player)
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        float time = (button.transform.localPosition.y + 0.097f) * 5;
+
+        button.DOPause();
+        button.DOLocalMove(Vector3.down * 0.097f, time)
+            .SetEase(Ease.Linear).OnUpdate(IsArrival);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(isActivated)
+        {
+            Trigger(false);
+        }
+
+        float time = (button.transform.localPosition.y + 0.097f) * 5;
+
+        button.DOPause();
+        button.DOLocalMove(Vector3.down * 0.097f, time)
+            .SetEase(Ease.Linear);
+    }
+
+    void IsArrival()
+    {
+        if(button.localPosition.y <= -1 && !isActivated)
+        {
+            isActivated = true;
+            button.DOPause();
+            Trigger(true);
+        }
+    }
+
+    void Trigger(bool activate)
+    {
+        if(activate)
+        {
+            for (int i = 0; i < on.Length; i++)
+            {
+                on[i].Invoke();
+            }
+        }
+        else
+        {
+            for (int i = 0; i < off.Length; i++)
+            {
+                on[i].Invoke();
+            }
+        }
+    }
 
 }
